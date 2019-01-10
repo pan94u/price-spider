ef4f84245a6bbc9dfb911fb3b7225dc0bd9afc5c
pan9 <sunzq@missfresh.cn>
2019-01-11 00:58:46 +0800

    报价跑完

--- main.js
+++ main.js
@@ -28,6 +28,51 @@ const getHtml = (path) => {
 
 getUrl().then((data) => {
     getHtml(data.path).then(html => {
-        console.log(html)
+        let $ = cheerio.load(html), modelText = []
+        $('.product-series-heading strong').each((i, title) => {modelText.push($(title).text())})
+        let content = $('.price-report tbody'), result = []
+        content.each((i, elem) => {
+            let title = $(elem).find('tr th'), price
+            price = $(elem).find('tr')
+            let fullData = handle(title ,price)
+            fullData.headText = modelText[i]
+            result.push(fullData)
+        })
+        console.log(result)
     })
 })
+
+function handle(title ,price) {
+    let titleArr = [], detail = []
+    title.each((i, elem) => {
+        let $ = cheerio.load(elem)
+        titleArr.push($(elem).text())
+    })
+    price.each((i, elem) => {
+        if(i==0){return}
+        let $ = cheerio.load(elem), tempArr = []
+        let chartsUrl, price=[], name, time
+        $(elem).find('td').each((i, td) => {
+            if($(td).attr('class') == 'model-name') {
+                name = $(td).text()
+                return
+            }
+            if($(td).attr('class') == 'time-ago') {
+                time = $(td).text()
+                return
+            }
+            if($(td).attr('class') == 'price-cell') {
+                price.push({
+                    color: title[i+1],
+                    num: $(td).text()
+                })
+                chartsUrl = $(td).find('a').attr('href')
+                return
+            }
+        })
+        detail.push(JSON.stringify({
+            chartsUrl,price,name,time
+        }))
+    })
+    return {detail}
+}
